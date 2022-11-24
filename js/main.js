60c177d8b2d2856c9f18f769f951f3c120b9cef7
Taeshin Park <pkthasin@gmail.com>
2022-11-24 17:33:19 +0900

    22-11-24 final

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 $(function(){
-   $(".main-visual-slide").bxSlider({
+   $(".slide_gall").bxSlider({
     auto:true,
-    pause:2000,
+    pause:1500,
     autoHover:true,
     autoControls:true,
     autoControlsCombine:true
    });
 
-   $("#notice-tab-wrap h4 a").on("click", tabmenu);
+//    $("#notice-tab-wrap h4 a").on("click", tabmenu);
 
-   function tabmenu(e){
-    e.preventDefault();
+//    function tabmenu(e){
+//     e.preventDefault();
 
-    var $ts = $(this);
-    var $next = $ts.parent().next();
+//     var $ts = $(this);
+//     var $next = $ts.parent().next();
 
-    if($next.is(":hidden")){
-        $("#notice-tab-wrap h4 a").removeClass("on");
-        $ts.addClass("on");
-        $("#notice-tab-wrap > div:visible").hide();
-        $next.show();
-    } // on을 토글하여 지금 메뉴를 숨기고 다음 메뉴를 보이게 함
+//     if($next.is(":hidden")){
+//         $("#notice-tab-wrap h4 a").removeClass("on");
+//         $ts.addClass("on");
+//         $("#notice-tab-wrap > div:visible").hide();
+//         $next.show();
+//     } // on을 토글하여 지금 메뉴를 숨기고 다음 메뉴를 보이게 함
 
-   }
+//    }
 
-   $(".grid").isotope({
-    itemSelector: '.grid-item',
-    layoutMode: 'fitRows'
-   });
+//    $(".grid").isotope({
+//     itemSelector: '.grid-item',
+//     layoutMode: 'fitRows'
+//    });
 });
